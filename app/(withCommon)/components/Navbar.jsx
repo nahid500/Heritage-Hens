831cc1b26f2a1cdb4835bcc4270cf9ecd05c18ad
Nahid Hossain <nahid95622@gmail.com>
2025-08-24 15:52:00 +0600

    added linls

--- app/(withCommon)/components/Navbar.jsx
+++ app/(withCommon)/components/Navbar.jsx
@@ -17,7 +17,7 @@ return (
 
         <div className="">
             <Link href='/' className="text-[#d97706] text-md hover:text-[#a85f0c] px-6">Home</Link>
-            <Link href='/about' className="text-[#d97706] text-md hover:text-[#a85f0c] px-6">About    </Link>
+            <Link href='/#about' className="text-[#d97706] text-md hover:text-[#a85f0c] px-6">About    </Link>
             <Link href='/products' className="text-[#d97706] text-md hover:text-[#a85f0c] px-6">Products</Link>
             <Link href='/contact' className="text-[#d97706] text-md hover:text-[#a85f0c] px-6">Contact    </Link>
         </div>
